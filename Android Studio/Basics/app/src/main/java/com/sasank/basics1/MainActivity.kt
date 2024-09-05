a7db719c8be844ab53190879cc48e44cc26724d8
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-09-05 22:34:25 +0530

    made a calculator layout

---
+++ Android Studio/Basics/app/src/main/java/com/sasank/basics1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.sasank.displaytextonclick
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.sasank.basics1.R
+
+open class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendNow(view: View) {
+        Toast.makeText(this, "Sending Now", Toast.LENGTH_SHORT).show()
+    }
+
+    fun receiveNow(view: View) {
+        Toast.makeText(this, "Receiving Now", Toast.LENGTH_SHORT).show()
+    }
+
+    fun deleteNow(view: View) {
+        Toast.makeText(this, "Deleting", Toast.LENGTH_SHORT).show()
+    }
+}
