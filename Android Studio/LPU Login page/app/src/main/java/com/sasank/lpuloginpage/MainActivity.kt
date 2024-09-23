8999cb8d01f1fc5142d1250f7094a6d44fd94b26
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-09-23 13:20:44 +0530

    added login functionality using kotlin

--- Android Studio/LPU Login page/app/src/main/java/com/sasank/lpuloginpage/MainActivity.kt
+++ Android Studio/LPU Login page/app/src/main/java/com/sasank/lpuloginpage/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.sasank.lpuloginpage
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var a=findViewById<EditText>(R.id.usrid)
+        var b=findViewById<EditText>(R.id.pass)
+        var c=findViewById<Button>(R.id.button)
+        c.setOnClickListener{
+            if(a.text.toString()=="12209302"&&b.text.toString()=="ABCD"){
+                Toast.makeText(this, "Logged in successfully", Toast.LENGTH_SHORT).show()
+                c.text="Logged in"
+            }
+            else{
+                Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show()
+                c.text="Enter the password again"
+            }
         }
     }
 }
