743ecde4060f7e93a4b1db47a330b655c0608da3
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2025-03-07 23:36:44 +0530

    updated KT file for Footer and Custom Toast

--- Footer/app/src/main/java/com/sasank/footer/MainActivity.kt
+++ Footer/app/src/main/java/com/sasank/footer/MainActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener {
+            menuItem->
+            when(menuItem.itemId){
+                R.id.home->{
+                    replaceFragment(Home())
+                    true
+                }
+                R.id.call->{
+                    replaceFragment(Call())
+                    true
+                }
+                R.id.location->{
+                    replaceFragment(Location())
+                    true
+                }
+                else->false
+            }
         }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout,fragment).commit()
     }
 }
